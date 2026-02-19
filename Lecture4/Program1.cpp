ea5228b579e6e6a11c12774d08c5466f9e65f5d0
Diamondluxe <hehediamondluxe@gmail.com>
2026-02-20 03:47:27 +0500

    Program1 of Lecture4

---
+++ Lecture4/Program1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age[10];
+    int total = 0;
+    int average;
+
+    for(int i=0; i<10; i++) {
+        cout << "Enter the age of student " << i+1 << " : ";
+        cin >> age[i];
+        total += age[i];
+    }
+
+    average = total / 10;
+    cout << "Average age of class is : " << average << endl;
+
+    return 0;
+}
